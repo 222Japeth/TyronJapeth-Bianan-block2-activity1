a347a55407d25c1a4cf9fbb1bc535597504f60ff
Japjap222 <japeth.bianan@gmail.com>
2023-12-09 23:48:27 +0800

    second

--- app/src/main/java/com/tyronjapeth/bianan/block1/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/tyronjapeth/bianan/block1/p1/quiz/MainActivity.kt
@@ -1,11 +1,40 @@
-package com.tyronjapeth.bianan.block1.p1.quiz
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editTextUsername: EditText
+    private lateinit var editTextPassword: EditText
+    private lateinit var buttonLogin: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editTextUsername = findViewById(R.id.editTextUsername)
+        editTextPassword = findViewById(R.id.editTextPassword)
+        buttonLogin = findViewById(R.id.buttonLogin)
+
+        buttonLogin.setOnClickListener {
+            val username = editTextUsername.text.toString()
+            val password = editTextPassword.text.toString()
+
+            // Replace these lines with your login logic
+            if (username == "your_username" && password == "your_password") {
+                // Successful login
+                showToast("Login successful")
+                // Here you might navigate to another activity or perform necessary actions
+            } else {
+                // Failed login
+                showToast("Invalid credentials")
+            }
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
